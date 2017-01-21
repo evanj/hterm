7169bfff3573c762a71e7b100afbab9c58de13cd
Evan Jones <ej@evanjones.ca>
2017-01-21 13:13:24 -0500

    htermshell: Use esc to embed static resources (not checked in)

--- cmd/htermshell/htermshell.go
+++ cmd/htermshell/htermshell.go
@@ -1,3 +1,4 @@
+//go:generate esc -pkg=$GOPACKAGE -o=static.go -prefix=static static
 package main
 
 import (
@@ -15,26 +16,33 @@ const gopathRelativeStaticDir = "src/github.com/evanj/hterm/cmd/htermshell/stati
 func main() {
 	addr := flag.String("addr", "localhost:8080", "Listening address e.g. :8080 for global")
 	cmd := flag.String("cmd", "bash -l", "Command to run (no shell variable expansion)")
+	gopathStatic := flag.Bool("gopathStatic", false, "Open static resources from $GOPATH")
 
 	flag.Parse()
 
 	starter := hterm.NewSubprocessStarter(strings.Split(*cmd, " "))
 	s := hterm.NewServer(starter)
 
-	gopath := ""
-	for _, env := range os.Environ() {
-		fmt.Println("WTF", env)
-		const gopathVar = "GOPATH="
-		if strings.HasPrefix(env, gopathVar) {
-			gopath = env[len(gopathVar):]
-			break
+	// Use the "real" http.FileSystem since we don't want to depend on the current working directory
+	var fs http.FileSystem
+	if *gopathStatic {
+		gopath := ""
+		for _, env := range os.Environ() {
+			const gopathVar = "GOPATH="
+			if strings.HasPrefix(env, gopathVar) {
+				gopath = env[len(gopathVar):]
+				break
+			}
 		}
-	}
-	if gopath == "" {
-		panic("Could not find GOPATH env var to locate static resources")
+		if gopath == "" {
+			panic("Could not find GOPATH env var to locate static resources")
+		}
+		fs = http.Dir(gopath + "/" + gopathRelativeStaticDir)
+	} else {
+		fs = FS(false)
 	}
 
-	staticHandler := http.FileServer(http.Dir(gopath + "/" + gopathRelativeStaticDir))
+	staticHandler := http.FileServer(fs)
 	http.Handle("/", staticHandler)
 	s.RegisterHandlers("/", http.DefaultServeMux)
 
